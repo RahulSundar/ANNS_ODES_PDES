3f44aa89d17b428227d2464a1157604772e5c7b7
Rahul Sundar <rahul.sundar95@gmail.com>
2021-03-20 17:09:33 +0530

    Numpy based activation functions

---
+++ Utils/Numpy/activation.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1.0 / (1 + np.exp(-(z)))
+
+
+def tanh(z):
+    return np.tanh(z)
+
+
+def sin(z):
+    return np.sin(z)
+
+
+def relu(z):
+    return np.maximum(0.001, z) 
+
+
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+
+def der_sigmoid(z):
+    #return sigmoid(z)*(1 - sigmoid(z))
+    return  (1.0 / (1 + np.exp(-(z))))*(1 -  1.0 / (1 + np.exp(-(z))))
+
+def der_tanh(z):
+    return 1 - np.tanh(z) ** 2
+
+def der_relu(z):
+    return (z>0)*1 + (z<0)*0.001 
